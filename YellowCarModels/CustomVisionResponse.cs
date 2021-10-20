6ef0885896ed8e6630edb0d24801228c4d5a09da
edalatpour <edalatpour@hotmail.com>
2021-10-19 22:13:14 -0400

    Initial commit

---
+++ YellowCarModels/CustomVisionResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YellowCar.Models
+{
+    public class CustomVisionResponse
+    {
+
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public string Created { get; set; }
+
+        public Prediction[] Predictions { get; set; }
+
+    }
+
+    public class Prediction
+    {
+
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public float Probability { get; set; }
+
+    }
+
+}
---
+++ YellowCarModels/YellowCarModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YellowCar.Models
+{
+    public class YellowCarModel
+    {
+
+        public YellowCarModel()
+        {
+            Who = "";
+            When = "";
+            Where = "";
+            ImageFileName = "";
+            IsYellowCar = false;
+            IsYellowHummer = false;
+        }
+
+        public string Who { get; set; }
+
+        public string When { get; set; }
+
+        public string Where { get; set; }
+
+        public string ImageFileName { get; set; }
+
+        public bool IsYellowCar { get; set; }
+
+        public bool IsYellowHummer { get; set; }
+
+    }
+
+}
